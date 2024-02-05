6db9210848c63ef063233fd1c53b8a8b6afa10ff
Federico Venturini <venturini.fe@gmail.com>
2024-02-05 20:34:00 +0000

    Update bar transitions and nav positioning

--- js/scripts.js
+++ js/scripts.js
@@ -188,23 +188,26 @@ $(document).ready(function () {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
-                    "padding": "35px 0"
+                    "padding": "25px 0"
                 });
                 $('header .member-actions').css({
-                    "top": "26px",
-                });
+                    "top": "25px",
+                })
+                $('header .navicon').css({
+                    "top": "30px",
+                });;
 
             } else {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
                     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
+                    "padding": "35px 0"
                 });
                 $('header .member-actions').css({
-                    "top": "41px",
+                    "top": "35px",
                 });
                 $('header .navicon').css({
-                    "top": "48px",
+                    "top": "42px",
                 });
             }
         });
