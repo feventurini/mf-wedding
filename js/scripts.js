cae523530436051c4f59f6c0477b00d390bbb1d7
Federico Venturini <venturini.fe@gmail.com>
2024-04-01 20:15:33 +0100

    Add honeymoon and gifts section

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ var current_lang = "en";
 var dictionary = {
     "en": {
         "_events": "Event",
-        "_where": "Where",
         "_venue": "Venues",
+        "_honeymoon": "Gifts",
         "_rsvp": "RSVP",
         "_intro": "We are getting married!",
         "_subtitle": "It is with immense joy that we invite you to celebrate with us the most special day of our lives.<br/>We are getting married on <b><i>9th November 2024</b></i> and we would like you to be a part of it",
@@ -15,15 +15,24 @@ var dictionary = {
         "_dress_code": "Dress Code",
         "_dress_code_header": "Dress Code",
         "_dress_code_item": "Formal",
-        "_dress_code_blurb": "For our wedding, we would prefer full formal/social attire. We kindly ask all our dear guests to dress according to the dress code to make this moment even more memorable. We can't wait to share this magical day with each and every one of you.<br/> \
-        <b>Women</b>: Long or midi dresses<br/> \
-        <b>Men</b>: Suit<br/> \
+        "_dress_code_blurb": "For our wedding, we would prefer full formal/social attire. We kindly ask all our dear guests to dress according to the dress code to make this moment even more memorable. We can't wait to share this magical day with each and every one of you.<br/>\
+        <b>Women</b>: Long or midi dresses<br/>\
+        <b>Men</b>: Suit<br/>\
         <b>Avoid</b>: Wearing white or dresses with white patterns (bride)",
         "_map_header": "How do I get there?",
         "_loc1": "Chiesa di San Benedetto",
         "_loc1_blurb": "<i>Via dell'Indipendenza, 64, Bologna</i>",
         "_loc2": "Palazzo Gnudi",
         "_loc2_blurb": "<i>Via Riva di Reno, 77, Bologna</i>",
+        "_honeymoon_header": "Gifts and Honeymoon",
+        "_honeymoon_blurb": "Dear friends and family,<br/>\
+        <br/>\
+        We are very happy to share our special moment with you! Your presence at our wedding is the greatest gift we could receive. As we have been together for six years and share our home (and a little cat), we have decided not to create a traditional gift list. Instead, we are planning a very special honeymoon to celebrate our life together. We will revisit our favorite country, Japan, where we got engaged, and also explore other destinations, such as South Korea, Honk Kong.<br/>\
+        <br/>\
+        If you wish to give us a gift in some way to make this trip even more special, we have created two accounts (PayPal and bank via Monzo) for contributions to this moment. Please do not hesitate to contact us if you have any questions.<br/>\
+        <br/>\
+        We can't wait to share this special day with all of you.<br/>\
+        With love, Marcella & Federico",
         "_rsvp_header": "RSVP?",
         "_rsvp_blurb": "We would greatly appreciate if you could RSVP before August 2024",
         // "_email": "Your email",
@@ -39,8 +48,8 @@ var dictionary = {
     },
     "it": {
         "_events": "Evento",
-        "_where": "Dove",
         "_venue": "Location",
+        "_honeymoon": "Regali",
         "_rsvp": "RSVP",
         "_intro": "Sei invitato al nostro matrimonio!",
         "_subtitle": "Con grande gioia vi invitiamo a celebrare con noi il giorno più speciale delle nostre vite.<br/>Ci sposeremo il <b><i>9 di Novembre 2024</b></i> e ti invitiamo a festeggiare con noi!",
@@ -50,15 +59,24 @@ var dictionary = {
         "_dress_code": "Dress Code",
         "_dress_code_header": "Dress Code",
         "_dress_code_item": "Formale",
-        "_dress_code_blurb": "Per il nostro matrimonio, preferiremmo un abbigliamento formale. Chiediamo gentilmente a tutti i nostri cari invitati di vestirsi secondo il dress code per rendere questo momento ancora più memorabile. Non vediamo l'ora di condividere questo giorno magico con ognuno di voi.<br/> \
-        <b>Donne</b>: Abiti lunghi o midi<br/> \
-        <b>Uomini</b>: Abito<br/> \
+        "_dress_code_blurb": "Per il nostro matrimonio, preferiremmo un abbigliamento formale. Chiediamo gentilmente a tutti i nostri cari invitati di vestirsi secondo il dress code per rendere questo momento ancora più memorabile. Non vediamo l'ora di condividere questo giorno magico con ognuno di voi.<br/>\
+        <b>Donne</b>: Abiti lunghi o midi<br/>\
+        <b>Uomini</b>: Abito<br/>\
         <b>Evitare</b>: Indossare bianco o abiti con stampa bianca (sposa)",
         "_map_header": "Come ci arrivo?",
         "_loc1": "Chiesa di San Benedetto",
         "_loc1_blurb": "<i>Via dell'Indipendenza, 64, Bologna</i>",
         "_loc2": "Palazzo Gnudi",
         "_loc2_blurb": "<i>Via Riva di Reno, 77, Bologna</i>",
+        "_honeymoon_header": "Regali e Luna di Miele",
+        "_honeymoon_blurb": "Cari amici e familiari,<br/>\
+        <br/>\
+        Siamo molto felici di condividere il nostro momento speciale con voi! La vostra presenza al nostro matrimonio è il regalo più grande che potremmo ricevere. Dato che siamo insieme da sei anni e condividiamo la nostra casa (e un piccolo gatto), abbiamo deciso di non creare una lista regali tradizionale. Invece, stiamo pianificando una luna di miele molto speciale per celebrare la nostra vita insieme. Rivisiteremo il nostro Paese preferito, il Giappone, dove ci siamo fidanzati, e esploreremo anche altre destinazioni, come la Corea del Sud e Hong Kong.<br/>\
+        <br/>\
+        Se desiderate darci un regalo in qualche modo per rendere questo viaggio ancora più speciale, abbiamo creato due conti (PayPal e banca tramite Monzo) per contributi a questo momento. Non esitate a contattarci se avete domande.<br/>\
+        <br/>\
+        Non vediamo l'ora di condividere questo giorno speciale con tutti voi.<br/>\
+        Con amore, Marcella & Federico",
         "_rsvp_header": "RSVP?",
         "_rsvp_blurb": "Apprezzeremmo una risposta prima di Agosto 2024",
         // "_email": "La tua email",
@@ -74,8 +92,8 @@ var dictionary = {
     },
     "pt": {
         "_events": "O Evento",
-        "_where": "Onde?",
         "_venue": "Local",
+        "_honeymoon": "Presentes",
         "_rsvp": "RSVP",
         "_intro": "Vamos nos casar",
         "_subtitle": "É com imensa alegria que convidamos vocês para celebrar conosco o dia mais especial de nossas vidas.<br/>O evento acontecerá dia <b><i>9 de Novembro de 2024</b></i>, e gostaríamos muito de ter você com a gente",
@@ -85,15 +103,23 @@ var dictionary = {
         "_dress_code": "Dress Code",
         "_dress_code_header": "Dress Code",
         "_dress_code_item": "Formal/social completo",
-        "_dress_code_blurb": "Pedimos gentilmente que todos os nossos queridos convidados estejam vestidos de acordo com o dress code para tornar este momento ainda mais memorável. Mal podemos esperar para compartilhar esse dia mágico com cada um de vocês.<br/> \
-        <b>Mulheres</b>: Vestidos longos ou midi<br/> \
-        <b>Homens</b>: Terno<br/> \
+        "_dress_code_blurb": "Pedimos gentilmente que todos os nossos queridos convidados estejam vestidos de acordo com o dress code para tornar este momento ainda mais memorável. Mal podemos esperar para compartilhar esse dia mágico com cada um de vocês.<br/>\
+        <b>Mulheres</b>: Vestidos longos ou midi<br/>\
+        <b>Homens</b>: Terno<br/>\
         <b>Evitar</b>: Uso de branco ou vestido com branco estampado (noiva)",
         "_map_header": "Como chegar",
         "_loc1": "Chiesa di San Benedetto",
         "_loc1_blurb": "<i>Via dell'Indipendenza, 64, Bologna</i>",
         "_loc2": "Palazzo Gnudi",
         "_loc2_blurb": "<i>Via Riva di Reno, 77, Bologna</i>",
+        "_honeymoon_header": "Presentes e Lua de Mel",
+        "_honeymoon_blurb": "Queridos amigos e familiares,<br/>\
+        <br/>\
+        Estamos muito felizes em compartilhar nosso momento especial com vocês! Sua presença em nosso casamento é o maior presente que poderíamos receber. Como já estamos juntos há seis anos e compartilhamos nossa casa (e uma gatinha), decidimos não criar uma lista de presentes tradicionais. Em vez disso, estamos planejando uma lua de mel muito especial para celebrar nossa vida juntos. Vamos revisitar nosso país favorito, o Japão, onde ficamos noivos, e também explorar outros destinos, como a Coréia do Sul.<br/>\
+        <br/>\
+        Se vocês desejarem nos presentear de alguma forma para tornar essa viagem ainda mais especial, criamos duas contas no PayPal e Monzo para vocês participarem desse momento. Não hesitem em entrar em contato conosco se tiverem alguma dúvida.<br/>\
+        Mal podemos esperar para compartilhar esse dia inesquecível com todos vocês.<br/>\
+        Com carinho, Marcella & Federico",
         "_rsvp_header": "RSVP?",
         "_rsvp_blurb": "Por favor, confirme antes do dia 1 de Agosto 2024",
         // "_email": "Seu e-mail",
@@ -108,6 +134,18 @@ var dictionary = {
         "_loading": "Aguarde um momento!"
     }
 };
+
+var honeymoon_items = [
+    "resources/img/honeymoon/IMG_6648.jpg",
+    "resources/img/honeymoon/IMG_6651.jpg",
+    "resources/img/honeymoon/IMG_6652.jpg",
+    "resources/img/honeymoon/IMG_6653.jpg",
+    "resources/img/honeymoon/IMG_6654.jpg",
+    "resources/img/honeymoon/IMG_6655.jpg",
+    "resources/img/honeymoon/IMG_6656.jpg",
+    "resources/img/honeymoon/IMG_6658.jpg",
+    "resources/img/honeymoon/IMG_6659.jpg"
+]
 
 
 $(document).ready(function () {
@@ -231,37 +269,6 @@ $(document).ready(function () {
         });
 
     });
-    
-    // /********************** Add to Calendar **********************/
-    // var myCalendar = createCalendar({
-    //     options: {
-    //         class: '',
-    //         // You can pass an ID. If you don't, one will be generated for you
-    //         id: ''
-    //     },
-    //     data: {
-    //         // Event title
-    //         title: "Federico and Marcella's Wedding",
-
-    //         // Event start date
-    //         start: new Date('Nov 27, 2017 10:00'),
-
-    //         // Event duration (IN MINUTES)
-    //         // duration: 120,
-
-    //         // You can also choose to set an end time
-    //         // If an end time is set, this will take precedence over duration
-    //         end: new Date('Nov 29, 2017 00:00'),
-
-    //         // Event Address
-    //         address: 'ITC Fortune Park Hotel, Kolkata',
-
-    //         // Event Description
-    //         description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
-    //     }
-    // });
-
-    // $('#add-to-cal').html(myCalendar);    
 
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
@@ -281,7 +288,6 @@ $(document).ready(function () {
 
             $.post('https://script.google.com/macros/s/AKfycbwuHpPyRUo5paqOWbDc-rwG3_wVHxb-IhqkCgWo4udo2K4VwgmRcUF__OO-P3d6gyrZ/exec', data)
                 .done(function (data) {
-                    console.log(data);
                     if (data.result === "error") {
                         $('#alert-wrapper').html(alert_markup('danger', data.message));
                     } else {
@@ -290,11 +296,12 @@ $(document).ready(function () {
                     }
                 })
                 .fail(function (data) {
-                    console.log(data);
                     $('#alert-wrapper').html(alert_markup('danger', dictionary[current_lang]['_server_issue']));
                 });
         }
     });
+
+    /********************** Localization **********************/
 
     var set_lang = function (dictionary) {
         $("[data-localized]").html(function () {
@@ -322,7 +329,22 @@ $(document).ready(function () {
     } else {
         set_lang(dictionary.en);
     }
-});
+
+    /********************** Honeymoon fund **********************/
+    chunkSize = 3;
+    for (let i = 0; i < honeymoon_items.length; i += chunkSize) {
+        var chunks = honeymoon_items.slice(i, i + chunkSize);
+        $("#honeymoon-carousel-inner").append(
+            `<div class="carousel-item row${i == 0 ? ' active' : ''}">
+                ${chunks.map(item => {
+                    return `<div class="col-4 float-left">
+                        <img src="${item}" class="img-fluid"></a>
+                    </div>`
+                }).join("\n")}
+            </div>`
+        )
+    }
+})
 
 /********************** Extras **********************/
 
@@ -330,3 +352,5 @@ $(document).ready(function () {
 function alert_markup(alert_type, msg) {
     return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
 }
+
+
